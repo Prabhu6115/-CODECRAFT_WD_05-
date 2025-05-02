950c82dd2609ed570cb9a4845ccb5177b9285ac8
Prabhu6115 <6115prabhu@gmail.com>
2025-05-02 16:03:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getWeather()
  {
-    const apiKey = '2f5c3473a7fdd12990c306df3cef1846';
+    const apiKey = 'your api key';
     const city = document.getElementById('city').value;
 
     if (!city) {
